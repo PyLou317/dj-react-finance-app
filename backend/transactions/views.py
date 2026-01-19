6b96d6b594d3fe61b943d94a2d472340bc6c3c93
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-01-18 21:52:40 -0800

    add sync simplefin views and add list views for org, account and transactions.

--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .tasks import sync_simplefin
+from .models import Organization, Account, Transaction
+from .serializers import OrganizationSerializer, AccountSerializer, TransactionSerializer
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+# SimpleFIN
+class TriggerLongSFSync(APIView):
+    def post(self, request):
+        sync_simplefin.delay()
+        return Response({"message": "Syncing transactions"}, status=202)
+
+class Trigger2DaySFSync(APIView):
+    def post(self, request):
+        sync_simplefin.delay()
+        return Response({"message": "Syncing transactions"}, status=202)
+    
+
+# Standard Views
+class ListOrganizationView(APIView):
+    def get(self, request, format=None):
+        orgs = Organization.objects.all()
+        serializer = OrganizationSerializer(orgs, many=True)
+        return Response(serializer.data)
+
+class ListAccountView(APIView):
+    def get(self, request, format=None):
+        accounts = Account.objects.all()
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data)
+
+class ListTransactionView(APIView):
+    def get(self, request, format=None):
+        trans = Transaction.objects.all()
+        paginator = PageNumberPagination()
+        page = paginator.paginate_queryset(trans, request)
+        
+        if page is not None:
+            serializer = TransactionSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        
+        serializer = TransactionSerializer(trans, many=True)
+        return Response(serializer.data)
