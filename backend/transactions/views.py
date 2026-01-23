178322ef5949c6508d08e19899e365b19560bedb
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-01-23 05:27:40 -0800

    add permissions to each view

--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .tasks import sync_simplefin
 from .models import Organization, Account, Transaction
 from .serializers import OrganizationSerializer, AccountSerializer, TransactionSerializer
 from rest_framework.pagination import PageNumberPagination
+
 
 # SimpleFIN
 class TriggerLongSFSync(APIView):
@@ -20,20 +22,26 @@ class Trigger2DaySFSync(APIView):
 
 # Standard Views
 class ListOrganizationView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request, format=None):
         orgs = Organization.objects.all()
         serializer = OrganizationSerializer(orgs, many=True)
         return Response(serializer.data)
 
 class ListAccountView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request, format=None):
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data)
 
 class ListTransactionView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request, format=None):
-        trans = Transaction.objects.all()
+        trans = Transaction.objects.filter(channel_owner=request.user)
         paginator = PageNumberPagination()
         page = paginator.paginate_queryset(trans, request)
         
