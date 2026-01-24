e73337eb18ecaf5f386f9b01d38b19f305fe8e2f
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-01-24 12:32:44 -0800

    customize admin

--- backend/transactions/admin.py
+++ backend/transactions/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(Account)
-admin.site.register(Organization)
-admin.site.register(Transaction)
+
+@admin.register(Organization)
+class OrgAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
     
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user', 'balance')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'payee', 'category')
+    list_display_links = ('id', 'payee',) 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'icon')
