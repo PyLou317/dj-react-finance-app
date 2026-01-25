f94a73e6021344854e6a86dff7cc2fbfb5a0d239
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-01-25 06:44:09 -0800

    feat: add dashboard page

---
+++ frontend/src/pages/dashboard/DashboardPage.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import { useAuth } from '@clerk/clerk-react';
+import CompanyLogo from '../../components/Logo';
+
+function DashboardPage() {
+
+  return (
+    <>
+      <p>Dashboard</p>
+    </>
+  );
+}
+
+export default DashboardPage;
