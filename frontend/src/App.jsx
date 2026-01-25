5dcd0dec79dccc4bff2ed601612636da50644dc4
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-01-25 06:41:28 -0800

    chore: format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,41 +4,23 @@ import {
   SignedOut,
   SignInButton,
   SignOutButton,
-  UserButton,
-  useAuth,
 } from '@clerk/clerk-react';
+import { Outlet } from 'react-router';
+import BottomNavbar from './components/navbar/BottomNavbar';
+import TopNavbar from './components/navbar/TopNavBar';
 
 function App() {
-  const { getToken } = useAuth();
-  const [accounts, setAccounts] = useState([]);
-
-  const fetchAccounts = async () => {
-    const token = await getToken();
-    const response = await fetch('http://127.0.0.1:8000/api/accounts/', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-    setAccounts(data);
-  };
-
   return (
     <>
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
-        <div>
-          <button onClick={fetchAccounts}>Fetch Accounts</button>
+        <TopNavbar />
+        <div className='mt-16 mb-16'>
+          <Outlet />
         </div>
-        <ul>
-          {accounts.map((account) => (
-            <li key={account.id}>{account.name}</li>
-          ))}
-        </ul>
-        <SignOutButton />
+        <BottomNavbar />
       </SignedIn>
     </>
   );
