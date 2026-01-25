873147a63ea0d49fd60b112df5ff19baace35b3f
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-01-25 06:43:41 -0800

    feat: add search bar

---
+++ frontend/src/components/searchbar/SearchBar.jsx
@@ -0,0 +1,11 @@
+export default function SearchBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        className="px-4 py-2 rounded-2xl border-2 border-gray-200 w-100"
+        placeholder="Search..."
+      />
+    </div>
+  );
+}
