7356e8487972ff2d6dd622d6b9ed32de6ae8c940
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 06:07:49 -0800

    style max width 500px

--- frontend/src/pages/dashboard/card.jsx
+++ frontend/src/pages/dashboard/card.jsx
@@ -1,6 +1,6 @@
 export default function ({ children }) {
   return (
-    <div className="mt-4 p-3 border border-gray-100 rounded-2xl shadow-md">
+    <div className="p-3 border w-full max-w-[500px] border-gray-100 bg-white rounded-2xl shadow-md">
       {children}
     </div>
   );
