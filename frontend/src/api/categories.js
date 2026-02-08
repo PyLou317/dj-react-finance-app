a0b64d1c13cad5cb49b1375eea8531f94b91e937
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 21:09:34 -0800

    add categories api functions

--- frontend/src/api/categories.js
+++ frontend/src/api/categories.js
@@ -1,14 +1,74 @@
-export const fetchCategoryTotals = async (token) => {
+export const fetchCurrentCategoryTotals = async (token) => {
   try {
-    const response = await fetch(`http://127.0.0.1:8000/api/category-totals/`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      `http://127.0.0.1:8000/api/current-category-totals/`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
       },
-    });
+    );
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch category totals: ${response.statusText}`);
+      throw new Error(
+        `Failed to fetch category totals: ${response.statusText}`,
+      );
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Network or Server Error:', error);
+    throw error;
+  }
+};
+
+export const fetchCategoryTotals = async (token, monthFilter, yearFilter) => {
+  const url = `/api/category-totals/?month=${monthFilter}&year=${yearFilter}`;
+
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8000/${url}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch category totals: ${response.statusText}`,
+      );
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Network or Server Error:', error);
+    throw error;
+  }
+};
+
+
+export const fetchCategories = async (token) => {
+  const url = `/api/categories`;
+
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8000/${url}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch categories: ${response.statusText}`,
+      );
     }
 
     return response.json();
