3a1b1ed19deede8934a5c20ae88378b166c9e92d
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 20:11:19 -0800

    import sum

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Sum
 
 
 class BankConnection(models.Model):
