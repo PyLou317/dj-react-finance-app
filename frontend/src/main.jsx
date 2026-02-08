1eac82f3dd332785b49131b96eeba8cfd23799fb
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 21:12:48 -0800

    add transaction detail view

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,16 +7,11 @@ import { ClerkProvider } from '@clerk/clerk-react';
 
 import { BrowserRouter, Routes, Route } from 'react-router';
 
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import DashboardPage from './pages/dashboard/DashboardPage';
-import TransactionsPage from './pages/transactions/TransactionsPage';
+import TransactionsPage from './pages/transactions/TransactionsPage.jsx';
+import TransactionDetailPage from './pages/transactions/TransactionDetailPage.jsx';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -34,7 +29,12 @@ createRoot(document.getElementById('root')).render(
           <Routes>
             <Route path="/" element={<App />}>
               <Route index element={<DashboardPage />} />
-              <Route path="transaction-list" element={<TransactionsPage />} />
+              <Route path="transaction-list" element={<TransactionsPage />}>
+                <Route
+                  path=":transactionId"
+                  element={<TransactionDetailPage />}
+                />
+              </Route>
             </Route>
           </Routes>
         </ClerkProvider>
