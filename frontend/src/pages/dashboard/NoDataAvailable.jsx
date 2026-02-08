6bca0c1f0cbadc9f8a4b8296658fb66e4ae5b6a6
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 21:14:13 -0800

    add no data available

---
+++ frontend/src/pages/dashboard/NoDataAvailable.jsx
@@ -0,0 +1,7 @@
+export default function NoDataAvailable() {
+  return (
+    <div className="flex items-center justify-center my-4 text-gray-500">
+      <p>No data available.</p>
+    </div>
+  );
+}
