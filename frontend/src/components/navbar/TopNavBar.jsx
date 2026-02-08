ad7f37cbb7a81280ab7027738a2fa5cc47419bf2
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 21:10:34 -0800

    add top navbar

--- frontend/src/components/navbar/TopNavBar.jsx
+++ frontend/src/components/navbar/TopNavBar.jsx
@@ -3,7 +3,7 @@ import WelcomeHeader from './Header';
 
 export default function TopNavbar() {
   return (
-    <nav className="top-0 left-0 right-0 pb-2 text-black bg-transparent z-100">
+    <nav className="top-0 left-0 right-0 pb-4 text-black bg-transparent z-100">
       <ul className="flex flex-row items-center justify-between gap-6 list-none">
         <WelcomeHeader />
         <li className="ml-auto list-none">
