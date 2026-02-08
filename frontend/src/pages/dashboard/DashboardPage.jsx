8ed1caef29f368e733e09794e7123206ca38affa
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 20:15:05 -0800

    add dashboard page

--- frontend/src/pages/dashboard/DashboardPage.jsx
+++ frontend/src/pages/dashboard/DashboardPage.jsx
@@ -1,17 +1,103 @@
-import { useState, useEffect } from 'react';
+import { useAuth } from '@clerk/clerk-react';
+import {
+  fetchCurrentCategoryTotals,
+  fetchCategoryTotals,
+} from '../../api/categories';
+import { fetchBudgets } from '../../api/budgets';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
 
+import TopNavbar from '../../components/navbar/TopNavBar';
 import BudgetCard from './BudgetCard';
 import AccountCardCarousel from './AccountCardCarousel';
 import TransCard from './TransCard';
+import CategoryCard from './CategoryCard';
+import MonthlyExpenseBarChart from './MonthlyExpenseBarChart';
+import { useState } from 'react';
 
-function DashboardPage() {
+export default function DashboardPage() {
+  const today = new Date();
+  const monthName = today.toLocaleString('default', { month: 'long' });
+  const year = today.getFullYear();
+
+  const [monthFilter, setMonthFilter] = useState(monthName);
+  const [yearFilter, setYearFilter] = useState(year);
+
+  const { getToken } = useAuth();
+
+  const {
+    isPending: budgetsIsPending,
+    isError: budgetsIsError,
+    data: budgets,
+    error: budgetsError,
+  } = useQuery({
+    queryKey: ['budgets'],
+    queryFn: async () => {
+      const token = await getToken();
+      return fetchBudgets(token);
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  const {
+    isPending: currentCategoryTotalsIsPending,
+    isError: currentCategoryTotalsIsError,
+    data: currentCategoryTotals,
+    error: currentCategoryTotalsError,
+  } = useQuery({
+    queryKey: ['currentCategoryTotals'],
+    queryFn: async () => {
+      const token = await getToken();
+      return fetchCurrentCategoryTotals(token);
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  const {
+    isPending: categoryTotalsIsPending,
+    isError: categoryTotalsIsError,
+    data: categoryTotals,
+    error: categoryTotalsError,
+  } = useQuery({
+    queryKey: ['categoryTotals', monthFilter, yearFilter],
+    queryFn: async () => {
+      const token = await getToken();
+      return fetchCategoryTotals(token, monthFilter, yearFilter);
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  function handleMonthFilter(month) {
+    setMonthFilter(month.target.value);
+  }
+
+  const years = [];
+  for (let i = year - 2; i <= year; i++) {
+    years.push(i);
+  }
+
+  function handleYearFilter(year) {
+    setYearFilter(year.target.value);
+  }
+
   return (
     <>
-      <AccountCardCarousel />
-      <BudgetCard />
-      <TransCard />
+      <TopNavbar />
+      <div className="mb-2">
+        <AccountCardCarousel />
+      </div>
+      <div className="flex flex-wrap gap-x-4 gap-y-2">
+        <BudgetCard budgets={budgets} categoryTotals={currentCategoryTotals} />
+        <CategoryCard categoryTotals={currentCategoryTotals} />
+        <TransCard />
+        <MonthlyExpenseBarChart
+          data={categoryTotals}
+          handleMonthFilter={handleMonthFilter}
+          handleYearFilter={handleYearFilter}
+          years={years}
+          monthFilter={monthFilter}
+          yearFilter={yearFilter}
+        />
+      </div>
     </>
   );
 }
-
-export default DashboardPage;
