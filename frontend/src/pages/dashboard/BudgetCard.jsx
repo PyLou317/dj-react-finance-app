8c8199d646a8f4c04408ae0c6174f1bae57f8976
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 21:13:43 -0800

    fetch budget data

--- frontend/src/pages/dashboard/BudgetCard.jsx
+++ frontend/src/pages/dashboard/BudgetCard.jsx
@@ -1,45 +1,12 @@
-import { useAuth } from '@clerk/clerk-react';
-import fetchBudgets from '../../api/budgets';
-import { fetchCategoryTotals } from '../../api/categories';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { capitalize } from '../../utils/capitalizeFirstLetter';
-import ProgressBar from './ProgressBar';
-import Card from './Card';
-import Title from './TItle';
 import { useMemo } from 'react';
-import isEqual from 'fast-deep-equal';
 
-export default function BudgetCard() {
-  const { getToken } = useAuth();
+import Card from './Card';
+import Title from './CardTItle';
+import ProgressBar from './ProgressBar';
+import NoDataAvailable from './NoDataAvailable';
 
-  const {
-    isPending: budgetsIsPending,
-    isError: budgetsIsError,
-    data: budgets,
-    error: budgetsError,
-  } = useQuery({
-    queryKey: ['budgets'],
-    queryFn: async () => {
-      const token = await getToken();
-      return fetchBudgets(token);
-    },
-    placeholderData: keepPreviousData,
-  });
-
-  const {
-    isPending: categoryTotalsIsPending,
-    isError: categoryTotalsIsError,
-    data: categoryTotals,
-    error: categoryTotalsError,
-  } = useQuery({
-    queryKey: ['categoryTotals'],
-    queryFn: async () => {
-      const token = await getToken();
-      return fetchCategoryTotals(token);
-    },
-    placeholderData: keepPreviousData,
-  });
-
+export default function BudgetCard({ budgets, categoryTotals }) {
   const mergedData = useMemo(() => {
     if (!budgets?.length || !categoryTotals?.length) return [];
 
@@ -52,7 +19,7 @@ export default function BudgetCard() {
     );
 
     // 2. Attach the spending to the budget
-    return budgets.map((budget) => {
+    return budgets?.map((budget) => {
       const categoryName = budget.category?.name?.toLowerCase();
       const spending = spendingLookup.get(categoryName) || 0;
 
@@ -69,32 +36,38 @@ export default function BudgetCard() {
   const title = monthName + ' ' + 'Budget';
   return (
     <Card>
-      <Title name={title} />
-      <ul className="flex flex-col gap-y-2">
-        {mergedData?.map((budget) => {
-          return (
-            <li key={budget.id} className="text-xs">
-              <span className="font-semibold">
-                {budget.category.parent
-                  ? capitalize(budget?.category?.parent.name) +
-                    ' ' +
-                    capitalize(budget?.category?.name)
-                  : capitalize(budget?.category?.name)}
-                :{' '}
-              </span>
-              {Math.abs(budget.spent).toLocaleString(undefined, {
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}{' '}
-              / {Math.trunc(budget.amount)}
-              <ProgressBar
-                current={Math.trunc(Math.abs(budget.spent))}
-                total={Math.trunc(budget.amount)}
-              />
-            </li>
-          );
-        })}
-      </ul>
+      <div className="flex flex-col">
+        <Title name={title} />
+        {mergedData?.length === 0 ? (
+          <NoDataAvailable />
+        ) : (
+          <ul className="flex flex-col gap-y-2">
+            {mergedData?.map((budget) => {
+              return (
+                <li key={budget.id} className="text-xs my-1">
+                  <span className="font-semibold">
+                    {budget.category.parent
+                      ? capitalize(budget?.category?.parent.name) +
+                        ' ' +
+                        capitalize(budget?.category?.name)
+                      : capitalize(budget?.category?.name)}
+                    :{' '}
+                  </span>
+                  {Math.abs(budget.spent).toLocaleString(undefined, {
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}{' '}
+                  / {Math.trunc(budget.amount)}
+                  <ProgressBar
+                    current={Math.trunc(Math.abs(budget.spent))}
+                    total={Math.trunc(budget.amount)}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
     </Card>
   );
 }
