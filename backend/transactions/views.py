bf9e5af26a6d59ddda1436e6362841648ac8f55b
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 20:17:02 -0800

    add transaction detail view

--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from django.db.models.functions import Coalesce
+from django.db.models import Sum, Q, Value, DecimalField
+
 from .tasks import sync_simplefin
 from .models import Organization, Account, Transaction, Budget, Category
-from .serializers import OrganizationSerializer, AccountSerializer, TransactionSerializer, BudgetSerializer, CategorySerializer
-from rest_framework.pagination import PageNumberPagination
+from .serializers import OrganizationSerializer, AccountSerializer, TransactionSerializer, TransactionWriteSerializer, BudgetSerializer, CategorySerializer
+
 from rest_framework import filters, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models import Sum, Q, Value, DecimalField
+
+from django.utils import timezone
 from datetime import date
-from django.db.models.functions import Coalesce
+
+from .tasks import daily_sync
+import datetime
 
 
 # SimpleFIN
@@ -45,14 +53,48 @@ class ListAccountView(APIView):
 class ListTransactionView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = TransactionSerializer
-    
+
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['account_id', 'category_id']
     search_fields = ['account__name', 'description', 'payee', 'category__name']
     
     def get_queryset(self):
         return Transaction.objects.filter(account__user=self.request.user)
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        
+        # Combine into one database hit
+        total = queryset.aggregate(
+            total=Sum('amount')
+        )['total'] or 0
 
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            response = self.get_paginated_response(serializer.data)
+            
+            response.data['total_sum'] = total
+            return response
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'total_sum': total,
+            'results': serializer.data
+        })
+        
+
+class TransactionDetailView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return TransactionWriteSerializer
+        return TransactionSerializer
+    
+    def get_queryset(self):
+        return Transaction.objects.filter(account__user=self.request.user)
+        
 
 class DashboardTransactionView(APIView):
     permission_classes = [IsAuthenticated]
