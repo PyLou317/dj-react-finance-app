5a191780d3609548de1ba777e1c0ae3cc06c8f64
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-07 20:13:18 -0800

    update to use react-router

--- frontend/src/pages/transactions/TransactionsPage.jsx
+++ frontend/src/pages/transactions/TransactionsPage.jsx
@@ -1,22 +1,10 @@
-import TransList from './TransList';
-import SearchBar from '../../components/searchbar/SearchBar';
-import Title from '../dashboard/TItle';
-import { useState } from 'react';
+import { Outlet, useParams } from 'react-router';
+import TransactionsListComponent from './TransactionsListComponent';
 
 export default function TransactionsPage() {
-  const [searchFilter, setSearchFilter] = useState('');
-
-  const handleSearch = (value) => {
-    setSearchFilter(value);
-  };
+  const { transactionId } = useParams();
 
   return (
-    <div className="px-3">
-      <Title name="Transactions" />
-      <div className="my-2">
-        <SearchBar onSearch={handleSearch} />
-      </div>
-      <TransList searchTerm={searchFilter} />
-    </div>
+    <div>{transactionId ? <Outlet /> : <TransactionsListComponent />}</div>
   );
 }
