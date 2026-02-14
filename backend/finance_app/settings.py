132f1cb81d18a749606c7df17fff293b24f7050e
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-13 21:05:24 -0800

    update staticfiles path

--- backend/finance_app/settings.py
+++ backend/finance_app/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = '/app/staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
