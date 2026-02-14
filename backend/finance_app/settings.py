d728b9e33ce57f4fc4252a9f5d2e5bca16f74fee
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-13 21:14:00 -0800

    update allowed host

--- backend/finance_app/settings.py
+++ backend/finance_app/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = [
+    'your-railway-app-name.up.railway.app', # Replace with your actual domain
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
