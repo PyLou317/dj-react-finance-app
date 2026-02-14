5f99b7e563d98dfb50c0f501502b7101223ffe0e
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-13 20:39:41 -0800

    update apiUrl

--- frontend/src/api/transactions.js
+++ frontend/src/api/transactions.js
@@ -1,5 +1,4 @@
-const VITE_API_URL = import.meta.env.VITE_API_URL;
-const apiUrl = VITE_API_URL || 'http://127.0.0.1:8000/';
+const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/';
 
 export const fetchTransactions = async (
   token,
