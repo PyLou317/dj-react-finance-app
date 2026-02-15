a1af5a34db458c481223ada6bde7eb1e3f1bde21
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-14 22:03:06 -0800

    update

--- backend/finance_app/settings.py
+++ backend/finance_app/settings.py
@@ -17,10 +17,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
@@ -32,7 +28,6 @@ ALLOWED_HOSTS = [
     'backend-production-89fc.up.railway.app',
     'localhost',
     '127.0.0.1',
-    '*',
 ]
 
 
