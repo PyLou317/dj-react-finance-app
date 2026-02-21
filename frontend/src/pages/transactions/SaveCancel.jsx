69ec1bab8620f4b9a7f3bdce8b810c7fd62c5409
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-20 22:13:07 -0800

    increase margin

--- frontend/src/pages/transactions/SaveCancel.jsx
+++ frontend/src/pages/transactions/SaveCancel.jsx
@@ -1,10 +1,10 @@
 export default function SaveCancelBtns({ onClick, isPending, cancel }) {
   return (
-    <div className="flex flex-row gap-1">
+    <div className="flex flex-row gap-1 m-1">
       <button
         id="saveBtn"
         onClick={onClick}
-        className="flex flex-row justify-center items-center gap-1 text-white bg-blue-500 px-2 rounded-2xl"
+        className="flex flex-row justify-center items-center gap-1 text-white bg-blue-500 py-1 px-3 rounded-2xl"
       >
         <p className="text-[9pt] cursor-pointer">
           {isPending ? 'Saving...' : 'Save'}
@@ -13,7 +13,7 @@ export default function SaveCancelBtns({ onClick, isPending, cancel }) {
       <button
         id="cancelBtn"
         onClick={cancel}
-        className="flex flex-row justify-center items-center gap-1 text-red-500 bg-white border border-red-400 px-2 rounded-2xl"
+        className="flex flex-row justify-center items-center gap-1 text-red-500 bg-white border border-red-400 py-1 px-3 rounded-2xl"
       >
         <p className="text-[9pt] cursor-pointer">Cancel</p>
       </button>
