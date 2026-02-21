b797a4b2f1b56eb24ae0960cb3a2462a7f665fd7
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-20 22:13:23 -0800

    make category clickable to edit

--- frontend/src/pages/categories/AddCategoryModal.jsx
+++ frontend/src/pages/categories/AddCategoryModal.jsx
@@ -54,7 +54,7 @@ export default function AddCategoryModal({ isOpen, setIsOpen, uniqueParents }) {
             type="text"
             name="name"
             value={formData.name}
-            onChange={setCategoryId()}
+            // onChange={setCategoryId()}
             required
             className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="e.g. Groceries"
