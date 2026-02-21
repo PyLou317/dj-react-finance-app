5b9e95b321bbd7db8dc2e6787c0806daa37bb68d
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-02-20 21:11:24 -0800

    change default state for account balance to not visible

--- frontend/src/pages/dashboard/AccountCard.jsx
+++ frontend/src/pages/dashboard/AccountCard.jsx
@@ -4,7 +4,9 @@ import CompanyLogo from '../../components/Logo';
 import { EyeClosed, Eye } from 'lucide-react';
 
 export default function AccountCard({ account }) {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+
+  console.log(account);
 
   if (account.balance == 0) {
     return null;
@@ -15,18 +17,21 @@ export default function AccountCard({ account }) {
   }
 
   return (
-    <div className="flex flex-col p-6 rounded-3xl min-w-60 min-h-50 border border-gray-400 bg-neutral-900 text-white justify-between">
-      <CompanyLogo
-        name={
-          account.org.name == "President's Choice Bank"
-            ? "Steve's No Frills"
-            : account?.org?.name
-        }
-        className="w-8 h-8"
-      />
-      <div className="flex flex-row justify-between">
-        <span className="flex flex-col text-lg">
-          Balance:{' '}
+    <div className="flex flex-col p-6 rounded-3xl md:min-w-80 min-w-60 min-h-50 border border-gray-400 bg-neutral-900 text-white justify-between">
+      <div className="flex flex-row gap-2">
+        <CompanyLogo
+          name={
+            account.org.name == "President's Choice Bank"
+              ? "Steve's No Frills"
+              : account?.org?.name
+          }
+          className="w-8 h-8"
+        />
+        <span>{account.org.name}</span>
+      </div>
+      <div className="flex flex-row justify-between items-start">
+        <span className="flex flex-col text-md">
+          Balance
           {isVisible ? (
             <span className="text-xl">
               {Number(account?.balance ?? 0).toLocaleString('en-CA', {
@@ -35,7 +40,7 @@ export default function AccountCard({ account }) {
               })}
             </span>
           ) : (
-            '* * * * *'
+            <span>* * * * *</span>
           )}
         </span>
         <button onClick={handleHideBalance} className="cursor-pointer">
