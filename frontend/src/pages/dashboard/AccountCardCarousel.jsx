0d657ae57cc69cd47d6566f3009b733097486acb
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-03-07 06:35:45 -0800

    add fall back state if no data on account card

--- frontend/src/pages/dashboard/AccountCardCarousel.jsx
+++ frontend/src/pages/dashboard/AccountCardCarousel.jsx
@@ -30,16 +30,29 @@ export default function AccountCardCarousel() {
     <>
       <Title name="My Accounts" />
       <div className="flex flex-row w-full gap-4 overflow-x-auto scrollbar-hide snap-x snap-mandatory">
-        {isPending || isFetching
-          ? skeletonCards.map((i) => <SkeletonAccountCard key={i} />)
-          : sortedAccounts?.map((account) => (
-              <AccountCard
-                key={account.id}
-                account={account}
-                isPending={isPending}
-                isFetching={isFetching}
-              />
-            ))}
+        {isPending || isFetching ? (
+          skeletonCards.map((i) => <SkeletonAccountCard key={i} />)
+        ) : sortedAccounts && sortedAccounts.length > 0 ? (
+          sortedAccounts?.map((account) => (
+            <AccountCard
+              key={account.id}
+              account={account}
+              isPending={isPending}
+              isFetching={isFetching}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col p-6 rounded-3xl min-w-80 min-h-50 border border-gray-400 bg-neutral-900 justify-between animate-pulse">
+            <div className="w-8 h-8 rounded-2xl bg-neutral-700"></div>
+            <div className="flex flex-row justify-between items-center">
+              <div className="space-y-2">
+                <div className="h-6 w-40 bg-neutral-600 rounded"></div>
+              </div>
+              <div className="size-6 rounded bg-neutral-700"></div>
+            </div>
+            <div className="h-4 w-48 bg-neutral-700 rounded"></div>
+          </div>
+        )}
       </div>
     </>
   );
