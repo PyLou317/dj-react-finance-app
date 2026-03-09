df110353b5412209044951543fbe161a7769e50a
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-03-08 19:59:39 -0700

    update account card when no data

--- frontend/src/pages/dashboard/AccountCardCarousel.jsx
+++ frontend/src/pages/dashboard/AccountCardCarousel.jsx
@@ -42,16 +42,41 @@ export default function AccountCardCarousel() {
             />
           ))
         ) : (
-          <div className="flex flex-col p-6 rounded-3xl min-w-80 min-h-50 border border-gray-400 bg-neutral-900 justify-between animate-pulse">
-            <div className="w-8 h-8 rounded-2xl bg-neutral-700"></div>
-            <div className="flex flex-row justify-between items-center">
-              <div className="space-y-2">
-                <div className="h-6 w-40 bg-neutral-600 rounded"></div>
+          <>
+            <div className="flex flex-col p-6 rounded-3xl min-w-80 min-h-50 border border-gray-400 bg-neutral-900 justify-between">
+              <div className="w-8 h-8 rounded-2xl bg-neutral-700"></div>
+              <div className="flex flex-row justify-between items-center">
+                <div className="space-y-2">
+                  <div className="h-6 w-40 text-gray-100 rounded">No Data</div>
+                </div>
               </div>
-              <div className="size-6 rounded bg-neutral-700"></div>
+              <div className="h-4 w-48 text-gray-100 rounded">
+                Please sync accounts
+              </div>
             </div>
-            <div className="h-4 w-48 bg-neutral-700 rounded"></div>
-          </div>
+            <div className="flex flex-col p-6 rounded-3xl min-w-80 min-h-50 border border-gray-400 bg-neutral-900 justify-between">
+              <div className="w-8 h-8 rounded-2xl bg-neutral-700"></div>
+              <div className="flex flex-row justify-between items-center">
+                <div className="space-y-2">
+                  <div className="h-6 w-40 text-gray-100 rounded">No Data</div>
+                </div>
+              </div>
+              <div className="h-4 w-48 text-gray-100 rounded">
+                Please sync accounts
+              </div>
+            </div>
+            <div className="flex flex-col p-6 rounded-3xl min-w-80 min-h-50 border border-gray-400 bg-neutral-900 justify-between">
+              <div className="w-8 h-8 rounded-2xl bg-neutral-700"></div>
+              <div className="flex flex-row justify-between items-center">
+                <div className="space-y-2">
+                  <div className="h-6 w-40 text-gray-100 rounded">No Data</div>
+                </div>
+              </div>
+              <div className="h-4 w-48 text-gray-100 rounded">
+                Please sync accounts
+              </div>
+            </div>
+          </>
         )}
       </div>
     </>
