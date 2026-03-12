1151ac3deb6eedf87e869c663223b80ca412a319
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-03-12 08:24:51 -0700

    Fix error with handle sync transactions button

--- frontend/src/pages/Settings/ConnectAccountPage.jsx
+++ frontend/src/pages/Settings/ConnectAccountPage.jsx
@@ -38,27 +38,30 @@ export default function ConnectAccountPage() {
       <h1 className="font-bold text-3xl mb-4">Sync Bank Data</h1>
       <button
         onClick={handleToggleModal}
-        disabled={syncTransMutation.isPending}
         className="flex p-8 bg-gray-50 rounded-xl items-center justify-center mb-4 border-2 border-gray-400 transition-colors cursor-pointer disabled:opacity-50 w-full hover:bg-teal-100 hover:border-teal-500"
       >
-        {syncTransMutation.isPending ? (
-          <>
-            <Loader size={4} />
-            <span>Syncing...</span>
-          </>
-        ) : (
-          <span>Click to Sync Bank Transactions</span>
-        )}
+        <span>Click to Sync Bank Transactions</span>
       </button>
       <Modal
         isOpen={isOpen}
         onClose={handleToggleModal}
         title="Sync Bank Transactions"
       >
-        <div className='flex flex-col items-center justify-center gap-y-4'>
+        <div className="flex flex-col items-center justify-center gap-y-4">
           <p>Agree to sync the last 6 days of bank transactions?</p>
-          <button className="bg-teal-400 px-3 py-1 rounded-xl text-white w-full hover:bg-teal-500">
-            Yes
+          <button
+            className="bg-teal-400 px-3 py-1 rounded-xl text-white w-full hover:bg-teal-500"
+            disabled={syncTransMutation.isPending}
+            onClick={handleSyncTransactions}
+          >
+            {syncTransMutation.isPending ? (
+              <>
+                <Loader size={4} />
+                <span>Syncing...</span>
+              </>
+            ) : (
+              <span>Yes</span>
+            )}
           </button>
         </div>
       </Modal>
