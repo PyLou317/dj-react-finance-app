b257c8f9d86e71c1266ecacc7e4b23aaa21e53b8
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-03-19 07:43:12 -0700

    update variable currentPage

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -30,7 +30,7 @@ export default function Pagination({
         &lt;
       </button>
       <button className="flex items-center justify-center h-10 w-10 bg-teal-500 text-white rounded-full">
-        {page}
+        {currentPage}
       </button>
       <button
         disabled={next === null}
