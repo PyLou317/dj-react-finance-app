b4ac070c0ff353748d6651912e00ad16cd2f69c7
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-03-19 07:54:13 -0700

    change color or prev and next page previews

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -30,7 +30,7 @@ export default function Pagination({
         &lt;
       </button>
       {currentPage > 1 && (
-        <button className="flex items-center justify-center h-10 w-10 bg-teal-500 text-white rounded-full">
+        <button className="flex items-center justify-center h-10 w-10 bg-teal-300 text-white rounded-full">
           {Number(currentPage) - 1}
         </button>
       )}
@@ -38,7 +38,7 @@ export default function Pagination({
         {Number(currentPage)}
       </button>
       {currentPage > 1 && (
-        <button className="flex items-center justify-center h-10 w-10 bg-teal-500 text-white rounded-full">
+        <button className="flex items-center justify-center h-10 w-10 bg-teal-300 text-white rounded-full">
           {Number(currentPage) + 1}
         </button>
       )}
