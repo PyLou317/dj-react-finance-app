4aea14222b155881bed6a3cd57f9b749a938a37d
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-04-13 21:13:57 -0700

    add fetch accounts by org

--- frontend/src/api/accounts.js
+++ frontend/src/api/accounts.js
@@ -1,6 +1,6 @@
 const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/';
 
-const fetchAccounts = async (token) => {
+export const fetchAccounts = async (token) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const response = await fetch(`${apiUrl}api/accounts/`, {
@@ -17,4 +17,36 @@ const fetchAccounts = async (token) => {
   return response.json();
 };
 
-export default fetchAccounts;
+export const fetchAccountsByOrg = async (token, accountId) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await fetch(`${apiUrl}api/filtered-accounts/${accountId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch accounts: ${response.statusText}`);
+  }
+
+  return response.json();
+};
+
+export const fetchInstitutions = async (token) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await fetch(`${apiUrl}api/organizations/`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch institutions: ${response.statusText}`);
+  }
+
+  return response.json();
+};
