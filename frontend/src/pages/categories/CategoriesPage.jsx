598952fdf77d41ba333e42426269f775cb1928b6
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-04-13 21:15:42 -0700

    update logic

--- frontend/src/pages/categories/CategoriesPage.jsx
+++ frontend/src/pages/categories/CategoriesPage.jsx
@@ -95,17 +95,22 @@ export default function CategoriesPage() {
       <div className="max-w-7xl mx-auto">
         {/* Page Header */}
         <div className="flex flex-col">
-          <div className="grid grid-cols-2 sm:flex-row sm:items-center sm:justify-between gap-4 mb-8">
+          <div className="grid grid-cols-2 sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
             <MainTitle name="Category Breakdown" />
             <button
-              className="flex items-center gap-2 max-w-[150px] ms-auto bg-teal-600 hover:bg-teal-700 text-white font-medium py-2 px-6 rounded-xl transition-colors cursor-pointer disabled:opacity-50"
+              className="flex items-center gap-2 md:min-w-75 ms-auto justify-center bg-teal-600 hover:bg-teal-700 text-white font-medium py-2 px-6 rounded-xl transition-colors cursor-pointer disabled:opacity-50"
               onClick={() => setIsOpen(true)}
             >
               Add Category
             </button>
           </div>
+        </div>
 
-          {/* Filter Dropdown Panel */}
+        <div className="bg-white p-6 rounded-2xl border border-gray-100 shadow-sm mb-4">
+          <h2 className="text-lg font-semibold text-gray-900 mb-6">
+            Spending Distribution
+          </h2>
+
           {categoryTotals?.length > 0 ? (
             <div className="bg-white p-4 rounded-2xl border border-gray-100 shadow-sm mb-6 grid grid-cols-1 sm:grid-cols-2 gap-4">
               <FilterWrapper
@@ -138,27 +143,27 @@ export default function CategoriesPage() {
               </FilterWrapper>
             </div>
           ) : null}
-        </div>
-
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-1">
-            <div className="bg-white p-6 rounded-2xl border border-gray-100 shadow-sm sticky top-6">
-              <h2 className="text-lg font-semibold text-gray-900 mb-6">
-                Spending Distribution
-              </h2>
+          <div className="grid grid-cols-2">
+            <div>
               {categoryTotals?.length > 0 ? (
                 <CategoryDonutChart categories={categoryTotals} />
               ) : (
                 <NoDataAvailable />
               )}
             </div>
+            <div></div>
           </div>
+        </div>
 
-          <div className="lg:col-span-2">
-            <div className="bg-white p-6 rounded-2xl border border-gray-100 shadow-sm">
-              <h2 className="text-lg font-semibold text-gray-900 mb-4">
-                Categories with Spending
-              </h2>
+        <div className="bg-white p-6 rounded-2xl border border-gray-100 shadow-sm">
+          <h2 className="text-xl font-semibold text-gray-900 mb-6">
+            Category Transactions
+          </h2>
+          <div>
+            <div>
+              <span className="font-semibold">Select Category:</span>
+            </div>
+            <div>
               {categoryTotalsIsPending ? (
                 <div className="text-center py-10 text-gray-500">
                   Loading...
@@ -188,52 +193,54 @@ export default function CategoriesPage() {
               ) : (
                 <NoDataAvailable />
               )}
-              {categoryTotals?.length > 0 ? (
-                <div className="mt-4 p-4">
-                  
-                  <div>
-                    {categoryDetails?.transactions.map((trans) => (
-                      <li
-                        key={trans.id}
-                        className="flex flex-row gap-4 p-2 items-center bg-white rounded-xl my-2 hover:scale-102 tooltip"
-                      >
-                        <div className="tooltip-content cursor-pointer truncate max-w-[250px]">
-                          {trans.notes != '' && trans.notes != ' '
-                            ? trans.notes
-                            : 'No notes'}
-                        </div>
-                        <CompanyLogo name={trans.payee} className="w-8 h-8" />
-                        <div className="flex flex-col min-w-0 flex-1">
-                          <span className="font-semibold truncate text-[14px]">
-                            {trans.payee}
-                          </span>
-                          {trans.category.parent ? (
-                            <span className="text-[14px] text-gray-400 uppercase tracking-wider truncate">
-                              {trans.category?.parent?.name} -{' '}
-                              {trans.category.name}
-                            </span>
-                          ) : (
-                            <span className="text-[14px] text-gray-400 uppercase tracking-wider truncate">
-                              {trans.category?.name}
-                            </span>
-                          )}
-                        </div>
-                        <span
-                          className={`ml-auto font-semibold text-[16px] ${trans.amount >= 0 ? 'text-green-500' : 'text-gray-900'}`}
-                        >
-                          {trans.amount >= 0 ? '+' : '-'}$
-                          {Math.abs(trans.amount).toLocaleString(undefined, {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2,
-                          })}
-                        </span>
-                      </li>
-                    ))}
-                  </div>
-                </div>
-              ) : null}
             </div>
           </div>
+          {categoryDetails != undefined ? (
+            <div className="mt-4 p-4">
+              <div>
+                {categoryDetails?.transactions.map((trans) => (
+                  <li
+                    key={trans.id}
+                    className="flex flex-row gap-4 p-2 items-center bg-white rounded-xl my-2 hover:scale-102 tooltip"
+                  >
+                    <div className="tooltip-content cursor-pointer truncate max-w-[250px]">
+                      {trans.notes != '' && trans.notes != ' '
+                        ? trans.notes
+                        : 'No notes'}
+                    </div>
+                    <CompanyLogo name={trans.payee} className="w-8 h-8" />
+                    <div className="flex flex-col min-w-0 flex-1">
+                      <span className="font-semibold truncate text-[14px]">
+                        {trans.payee}
+                      </span>
+                      {trans.category.parent ? (
+                        <span className="text-[14px] text-gray-400 uppercase tracking-wider truncate">
+                          {trans.category?.parent?.name} - {trans.category.name}
+                        </span>
+                      ) : (
+                        <span className="text-[14px] text-gray-400 uppercase tracking-wider truncate">
+                          {trans.category?.name}
+                        </span>
+                      )}
+                    </div>
+                    <span
+                      className={`ml-auto font-semibold text-[16px] ${trans.amount >= 0 ? 'text-green-500' : 'text-gray-900'}`}
+                    >
+                      {trans.amount >= 0 ? '+' : '-'}$
+                      {Math.abs(trans.amount).toLocaleString(undefined, {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                      })}
+                    </span>
+                  </li>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div className="text-center py-10">
+              <span>Please select a category to view data</span>
+            </div>
+          )}
         </div>
       </div>
 
